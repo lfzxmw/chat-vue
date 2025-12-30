67f6dc9170b2c3bf2cb5225d1fff1b8caeb022d3
fanliu12 <fanliu12@iflytek.com>
2025-12-30 11:08:40 +0800

    fix: use relative base path

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   // 当部署到 GitHub Pages 时，base 需要设置为 /<repo-name>/
-  base: process.env.NODE_ENV === 'production' ? '/chat-vue/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? './' : './',
   plugins: [vue()],
+  base: './',  // 关键！改成相对路径，最通用、最稳
   build: {
     outDir: 'dist'
   }
